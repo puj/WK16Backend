5c2c1dd3bc07b1232be8ec5b46e8482244e07d40
Van <vango85@gmail.com>
2020-11-30 14:40:05 +0100

    Fixed typo

--- rooms.js
+++ rooms.js
@@ -1,114 +1,114 @@
 export const rooms = {
   1: {
-    coordinates: "0,0",
+    coordinates: '0,0',
     description:
-      "You find yourself in under a large archway opening into a cavern.  A sense of purpose fills you.",
+      'You find yourself in under a large archway opening into a cavern.  A sense of purpose fills you.',
     actions: [
       {
-        type: "move",
-        direction: "East",
+        type: 'move',
+        direction: 'East',
         description:
           "A worn sign 'The Temple of *ech*igo'. Some of the letters are missing. An overgrown paved path leads to the East",
       },
     ],
   },
   2: {
-    coordinates: "1,0",
+    coordinates: '1,0',
     description: "It's warmer here.  Rays of light penetrate the the ceiling.",
     actions: [
       {
-        type: "move",
-        direction: "North",
+        type: 'move',
+        direction: 'North',
         description:
           "A rickety bridge crosses a large cavern to the north. You see strange symbols '<' '>' '/' are engraved on the planks of the bridge.",
       },
       {
-        type: "move",
-        direction: "West",
-        description: "You see a large archeway in the distance",
+        type: 'move',
+        direction: 'West',
+        description: 'You see a large archeway in the distance',
       },
     ],
   },
   3: {
-    coordinates: "1,1",
+    coordinates: '1,1',
     description:
-      "The walls of the cavern seem to grow brighter and more colorful here. ",
+      'The walls of the cavern seem to grow brighter and more colorful here. ',
     actions: [
       {
-        type: "move",
-        direction: "South",
+        type: 'move',
+        direction: 'South',
         description:
-          "The bridge somehow looks sturdier now.  Seeing it feels you with confidence.",
+          'The bridge somehow looks sturdier now.  Seeing it feels you with confidence.',
       },
       {
-        type: "move",
-        direction: "West",
+        type: 'move',
+        direction: 'West',
         description:
-          "A clear colorful pattern now marks the clearly cut stone pavers leading to the West",
+          'A clear colorful pattern now marks the clearly cut stone pavers leading to the West',
       },
     ],
   },
   4: {
-    coordinates: "0,1",
+    coordinates: '0,1',
     description:
-      "You emerge into a large, four-walled room with mechanical gadgets and bronze metal gizmos laying in a mess on the floor. They seem to have once been part of some larger moving mechanism.",
+      'You emerge into a large, four-walled room with mechanical gadgets and bronze metal gizmos laying in a mess on the floor. They seem to have once been part of some larger moving mechanism.',
     actions: [
       {
-        type: "move",
-        direction: "North",
+        type: 'move',
+        direction: 'North',
         description:
-          "You hear faint sounds -- clicks and buzzing mechanical noises echoing on the other side of a door.",
+          'You hear faint sounds -- clicks and buzzing mechanical noises echoing on the other side of a door.',
       },
       {
-        type: "move",
-        direction: "East",
+        type: 'move',
+        direction: 'East',
         description:
-          "The colorful pathway to the East puts your mind at ease.  You seem to understand the intention of the one who created it.",
+          'The colorful pathway to the East puts your mind at ease.  You seem to understand the intention of the one who created it.',
       },
     ],
   },
   5: {
-    coordinates: "0,2",
+    coordinates: '0,2',
     description:
-      "As you move through the room, the walls change colors with every step you take. The buzzing and clicking from before now seem to form one harmonious melody.  They respond to your every movement.",
+      'As you move through the room, the walls change colors with every step you take. The buzzing and clicking from before now seem to form one harmonious melody.  They respond to your every movement.',
     actions: [
       {
-        type: "move",
-        direction: "South",
+        type: 'move',
+        direction: 'South',
         description:
-          "Through the door you see that the gizmos, previously scattered on the floor, have formed one complete, whirring machine.  ",
+          'Through the door you see that the gizmos, previously scattered on the floor, have formed one complete, whirring machine.  ',
       },
       {
-        type: "move",
-        direction: "North",
+        type: 'move',
+        direction: 'North',
         description:
-          "Looking to the North, you see an arch leading to room containing a large chest.  Papers and scrolls are stuck in the mouth of the ornate container, as if someone store them in a hurry. Empty bookshelves line the room",
+          'Looking to the North, you see an arch leading to room containing a large chest.  Papers and scrolls are stuck in the mouth of the ornate container, as if someone store them in a hurry. Empty bookshelves line the room',
       },
     ],
   },
   6: {
-    coordinates: "0,3",
+    coordinates: '0,3',
     description:
-      "The mouth of the chest swings open. The papers spring from the chest, whirling around your head.  You shield your face, but an instant later all is calm.  The papers, books and scrolls from the chest are now neatly arranged on the bookshelves around the room.",
+      'The mouth of the chest swings open. The papers spring from the chest, whirling around your head.  You shield your face, but an instant later all is calm.  The papers, books and scrolls from the chest are now neatly arranged on the bookshelves around the room.',
     actions: [
       {
-        type: "move",
-        direction: "South",
+        type: 'move',
+        direction: 'South',
         description:
-          "A calm, faint melody flows into the room from the archway to the South.  Colors bounce calmly through the threshold. ",
+          'A calm, faint melody flows into the room from the archway to the South.  Colors bounce calmly through the threshold. ',
       },
       {
-        type: "move",
-        direction: "East",
+        type: 'move',
+        direction: 'East',
         description:
-          "You see a bright light through an opening in the wall to the east. ",
+          'You see a bright light through an opening in the wall to the east. ',
       },
     ],
   },
   7: {
-    coordinates: "1,3",
+    coordinates: '1,3',
     description:
-      "As you step into the light, you feel an immesnse relief and calm flow over you. It feels like an important journey has ended, but you get the sense that a new one is just beginning...",
+      'As you step into the light, you feel an immense relief and calm flow over you. It feels like an important journey has ended, but you get the sense that a new one is just beginning...',
     actions: [],
   },
 };
